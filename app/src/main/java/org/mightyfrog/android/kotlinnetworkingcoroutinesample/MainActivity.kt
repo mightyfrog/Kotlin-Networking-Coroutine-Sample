33ebe66ccf75112a45918cc12eab99f2e05e89dc
mightyfrog <mightyfrog.gc@gmail.com>
2018-09-01 00:36:39 +0900

    add another sample

--- app/src/main/java/org/mightyfrog/android/kotlinnetworkingcoroutinesample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/kotlinnetworkingcoroutinesample/MainActivity.kt
@@ -5,9 +5,11 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.experimental.DefaultDispatcher
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
 import org.json.JSONObject
 import java.net.URL
 import java.util.*
@@ -23,9 +25,15 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener {
+//            launch(UI) {
+//                activityCircle.visibility = View.VISIBLE
+//                textView.text = parse(load().await()).await()
+//                activityCircle.visibility = View.GONE
+//            }
+
             launch(UI) {
                 activityCircle.visibility = View.VISIBLE
-                textView.text = parse(load().await()).await()
+                textView.text = withContext(DefaultDispatcher) { parse2(load2()) }
                 activityCircle.visibility = View.GONE
             }
         }
@@ -48,5 +56,24 @@ class MainActivity : AppCompatActivity() {
                     .append(("\n"))
         }
         return@async sb.toString()
+    }
+
+    private fun load2(): String {
+        val url = URL("https://www.reddit.com/.json")
+        url.openStream().use { stream ->
+            return Scanner(stream).useDelimiter("\\A").next()
+        }
+    }
+
+    private fun parse2(json: String): String {
+        val jo = JSONObject(json).getJSONObject("data").getJSONArray("children")
+        val len = jo.length()
+        val sb = StringBuilder()
+        for (i in 0 until len) {
+            sb.append(jo.getJSONObject(i).getJSONObject("data").getString("title"))
+                    .append(("\n"))
+                    .append(("\n"))
+        }
+        return sb.toString()
     }
 }
